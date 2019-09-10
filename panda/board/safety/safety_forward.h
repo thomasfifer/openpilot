4f86ce1123d8559286cfc1d2cc10ce20fe74952b
Thomas Fifer <thomasfiferjr@gmail.com>
2019-09-09 21:46:35 -0400

    890c02f

--- panda/board/safety/safety_forward.h
+++ panda/board/safety/safety_forward.h
@@ -1,9 +1,6 @@
-
 // Stores the array index of a matched car fingerprint/forwarding profile
 int enabled = -1;
-int MDPS12_checksum = -1;
-int MDPS12_cnt = 0;   
-int last_StrColT = 0;
+
 
 static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
@@ -23,27 +20,6 @@ static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   if ((addr == 832) && (bus == hyundai_camera_bus) && (hyundai_camera_detected != 1)) {
     hyundai_giraffe_switch_2 = 1;
   }
-  if (addr == 593) {
-    if (MDPS12_checksum == -1) {
-      int New_Chksum2 = 0;
-      uint8_t dat[8];
-      for (int i=0; i<8; i++) {
-        dat[i] = GET_BYTE(to_push, i);
-        }
-      int Chksum2 = dat[3];
-      dat[3] = 0;
-      for (int i=0; i<8; i++) {
-        New_Chksum2 += dat[i];
-        }
-      New_Chksum2 %= 256;
-      if (Chksum2 == New_Chksum2) {
-      MDPS12_checksum = 1;
-      }
-      else {
-      MDPS12_checksum = 0;
-      }
-    }
-  } 
   if ((enabled != 1) && (hyundai_camera_detected != 1) && (hyundai_giraffe_switch_2 == 1)) {
     safety_cb_enable_all();
     // begin forwarding with that profile
@@ -58,66 +34,8 @@ static void forward_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 }
 
 static int forward_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
-  int addr = GET_ADDR(to_send);
+  UNUSED(to_send);
   if (enabled == 1) {
-    if (addr == 593) {
-      
-      uint8_t dat[8];
-      int New_Chksum2 = 0;
-      for (int i=0; i<8; i++) {
-        dat[i] = GET_BYTE(to_send, i);
-      }
-      if (MDPS12_cnt > 330) {
-        int StrColTq = dat[0] | (dat[1] & 0x7) << 8;
-        int OutTq = dat[6] >> 4 | dat[7] << 4;
-        if (MDPS12_cnt == 331) {
-          StrColTq -= 164;
-        } else {
-          StrColTq = last_StrColT + 34;
-        }
-        OutTq = 2058;
-
-        dat[0] = StrColTq & 0xFF;
-        dat[1] &= 0xF8;
-        dat[1] |= StrColTq >> 8;
-        dat[6] &= 0xF;
-        dat[6] |= (OutTq & 0xF) << 4;
-        dat[7] = OutTq >> 4;
-            
-
-        to_send->RDLR &= 0xFFF800;
-        to_send->RDLR |= StrColTq;
-        to_send->RDHR &= 0xFFFFF;
-        to_send->RDHR |= OutTq << 20;
-        last_StrColT = StrColTq;
-        }
-      dat[3] = 0;
-      if (MDPS12_checksum) { 
-        for (int i=0; i<8; i++) {
-          New_Chksum2 += dat[i];
-        }
-        New_Chksum2 %= 256;
-
-      } else if (!MDPS12_checksum) { //we need CRC8 checksum
-        uint8_t crc = 0xFD;
-        uint16_t poly = 0x11D;
-        int i, j;
-        for (i=0; i<8; i++){
-          crc ^= dat[i];
-          for (j=0; j<8; j++) {
-            if ((crc & 0xDF) != 0U) {
-              crc = (uint8_t)((crc << 1) ^ poly);
-            } else {
-              crc <<= 1;
-            }
-          }
-        }
-        New_Chksum2 = crc;
-      }
-      to_send->RDLR |= New_Chksum2 << 24;
-      MDPS12_cnt += 1;
-      MDPS12_cnt %= 345;
-      }
       // must be true for fwd_hook to function
       return 1;
   }
@@ -129,10 +47,13 @@ static int forward_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   int bus_fwd = -1;
   if (enabled == 1) {
     if (bus_num == 0) {
-      bus_fwd = hyundai_camera_bus;
+      bus_fwd = hyundai_camera_bus + 10;
     }
     if (bus_num == hyundai_camera_bus) {
-      bus_fwd = 0;
+      bus_fwd = 0 + 10;
+    }
+    if (bus_num == 1) {
+      bus_fwd = 0 + 20;
     }
   }
   return bus_fwd;
