7e64b44ff407a9be2c0770cb7ebc36cc9de5729c
Thomas Fifer <thomasfiferjr@gmail.com>
2019-10-20 20:29:00 -0400

    lower limits

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -12,7 +12,7 @@ from selfdrive.can.packer import CANPacker
 class SteerLimitParams:
   STEER_MAX = 255   # 409 is the max, 255 is stock
   STEER_DELTA_UP = 3
-  STEER_DELTA_DOWN = 7
+  STEER_DELTA_DOWN = 5
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
