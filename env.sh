7d633c046271b2c969a5bbe3d046836e0fca502f
Thomas Fifer <thomasfiferjr@gmail.com>
2026-07-17 21:05:03 -0400

    🖕 comma's broken fingerprint system

--- env.sh
+++ env.sh
@@ -5,6 +5,9 @@ if [ -f .env ]; then
   set -a && source .env && set +a
 fi
 
+export FINGERPRINT="VOLVO_S60_RECHARGE"
+export SKIP_FW_QUERY=1
+
 # Set Konik API endpoints if USE_KONIK is enabled
 if [ "$USE_KONIK" = "1" ]; then
   export API_HOST=https://api.konik.ai
